ff7d0e5d1bf21a7be75335ce951e147d8657188a
苅宿直樹 <karishukunaoki@kariyadonaokinoMacBook-Air.local>
2026-05-19 14:00:26 +0900

    貸出登録、在庫スケジュール削除_2

--- src/main/java/jp/co/metateam/library/constants/Constants.java
+++ src/main/java/jp/co/metateam/library/constants/Constants.java
@@ -2,10 +2,10 @@ package jp.co.metateam.library.constants;
 
 public class Constants {
     
-    /** 在庫ステータス：利用可 */
+    /** 保管状態：利用可 */
     public static final int STOCK_AVAILABLE = 0;
 
-    /** 在庫ステータス：利用不可 */
+    /** 保管状態：利用不可 */
     public static final int STOCK_UNAVAILABLE = 1;
 
 }
